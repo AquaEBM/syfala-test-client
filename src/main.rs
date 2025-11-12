b9bf593499d642d80d9bd91e086fc038a6209fdd
AquaEBM <AquaEBM@gmail.com>
2025-11-12 12:22:01 +0100

    more simplification

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,15 @@ fn as_bytes<T>(data: &[T]) -> &[u8] {
 }
 
 const PORT: u16 = 6910;
-const RB_MIN_NUM_FRAMES: num::NonZeroUsize = num::NonZeroUsize::new(1 << 9).unwrap();
+
+// This has to be big enough to accomodate at least a couple of process cycles
+// Needless to say, those can have very high buffer sizes. One second is usually enough
+const RB_SIZE_FRAMES: num::NonZeroUsize = num::NonZeroUsize::new(1 << 16).unwrap();
+
+const MAX_SPLS_PER_DATAGRAM: num::NonZeroUsize = num::NonZeroUsize::new(362).unwrap();
 
 // 1
-const DEFAULT_NUM_PORTS: num::NonZeroUsize = num::NonZeroUsize::MIN;
+const DEFAULT_N_PORTS: num::NonZeroUsize = num::NonZeroUsize::MIN;
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct JackBufPtr(NonNull<f32>);
@@ -54,47 +59,14 @@ fn read_exact_array<const N: usize>(reader: &mut (impl Read + ?Sized)) -> io::Re
     reader.read_exact(&mut buf).map(|()| buf)
 }
 
-fn request_connection_to_syfala_server(
-    addr: &core::net::SocketAddr,
-    num_ports: &mut usize,
-) -> io::Result<(num::NonZeroUsize, std::net::TcpStream)> {
-    println!("Attempting to connect to SyFaLa Server at {addr}...");
-    let mut stream = std::net::TcpStream::connect(&addr)?;
-    stream.set_nodelay(true)?;
-
-    println!("Success!\n");
-
-    println!("Requesting {num_ports} ports...");
-    stream.write_all(&num_ports.to_be_bytes())?;
-
-    let num_available_ports = usize::from_be_bytes(read_exact_array(&mut stream)?);
-
-    match num_available_ports.cmp(num_ports) {
-        cmp::Ordering::Less => {
-            println!("WARNING: server inputs overflow, found {num_available_ports} ports instead\n")
-        }
-        cmp::Ordering::Equal => println!("Accepted!\n"),
-        _ => unreachable!("INTERNAL ERROR: server returned invalid port count"),
-    }
-
-    *num_ports = num_available_ports;
-
-    let chunk_size_frames = num::NonZeroUsize::new(usize::from_be_bytes(read_exact_array(&mut stream)?))
-        .expect("INTERNAL ERROR: Server returned a chunk size of 0 frames");
-
-    println!("Server requested {chunk_size_frames} frames of buffering\n");
-
-    Ok((chunk_size_frames, stream))
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = std::env::args().skip(1);
 
-    let mut num_ports = args
+    let n_ports_request = args
         .next()
         .as_deref()
         .map(|s| s.parse().unwrap())
-        .unwrap_or(DEFAULT_NUM_PORTS.get());
+        .unwrap_or(DEFAULT_N_PORTS.get());
 
     let addr = core::net::SocketAddr::new(
         args.next()
@@ -104,9 +76,33 @@ fn main() -> Result<(), Box<dyn Error>> {
         PORT,
     );
 
-    let (chunk_size, stream) = request_connection_to_syfala_server(&addr, &mut num_ports)?;
-    let Some(num_ports) = num::NonZeroUsize::new(num_ports) else {
-        return Err("ERROR: creating a client with 0 ports".into());
+    println!("Attempting to connect to SyFaLa Server at {addr}...");
+    let mut stream = std::net::TcpStream::connect(&addr)?;
+    stream.set_nodelay(true)?;
+
+    println!("Success!\n");
+
+    println!("Requesting {n_ports_request} ports...");
+    stream.write_all(&n_ports_request.to_be_bytes())?;
+
+    let n_ports = usize::from_be_bytes(read_exact_array(&mut stream)?);
+
+    match n_ports.cmp(&n_ports_request) {
+        cmp::Ordering::Less => println!("WARNING: Found {n_ports} ports instead\n"),
+        cmp::Ordering::Equal => println!("Accepted!\n"),
+        cmp::Ordering::Greater => {
+            unreachable!("INTERNAL ERROR: server returned invalid port count")
+        }
+    }
+
+    let chunk_size_frames =
+        num::NonZeroUsize::new(usize::from_be_bytes(read_exact_array(&mut stream)?))
+            .expect("INTERNAL ERROR: Server returned a chunk size of 0 frames");
+
+    println!("Server suggests {chunk_size_frames} frames of buffering\n");
+
+    let Some(n_ports) = num::NonZeroUsize::new(n_ports) else {
+        return Err("Attempted to create a client with 0 ports".into());
     };
 
     println!("Creating UDP Socket...");
@@ -114,35 +110,38 @@ fn main() -> Result<(), Box<dyn Error>> {
     socket.connect(&addr)?;
     println!("Success!\n");
 
-    let chunk_size_spls = chunk_size.checked_mul(num_ports).unwrap();
+    let chunk_size_spls = chunk_size_frames
+        .checked_mul(n_ports)
+        .unwrap()
+        .min(MAX_SPLS_PER_DATAGRAM);
 
-    let rb_size_frames = num::NonZeroUsize::new(
-        RB_MIN_NUM_FRAMES
-            .get()
-            .checked_next_multiple_of(chunk_size.get())
-            .unwrap(),
-    )
-    .unwrap();
-
-    let rb_size_spls = rb_size_frames.checked_mul(num_ports).unwrap();
+    let rb_size_spls = RB_SIZE_FRAMES
+        .checked_mul(n_ports)
+        .unwrap()
+        .get()
+        .checked_next_multiple_of(chunk_size_spls.get())
+        .unwrap();
 
     println!("Allocating Ring Buffer. Size = {rb_size_spls} samples");
 
-    let (mut tx, mut rx) = rtrb::RingBuffer::new(rb_size_spls.get());
+    let (mut tx, mut rx) = rtrb::RingBuffer::new(rb_size_spls);
 
     let network_thread = std::thread::current();
 
-    let (jack_client, _status) =
-        jack::Client::new("CLIENT", jack::ClientOptions::NO_START_SERVER).unwrap();
+    let (jack_client, _status) = jack::Client::new(
+        &format!("SyFaLa {} | {}", addr.ip(), addr.port()),
+        jack::ClientOptions::NO_START_SERVER,
+    )
+    .unwrap();
 
-    let ports = Box::from_iter((1..=num_ports.get()).map(|i| {
+    let ports = Box::from_iter((1..=n_ports.get()).map(|i| {
         jack_client
             .register_port(&format!("input_{i}"), jack::AudioIn::default())
             .unwrap()
     }));
 
     let mut port_buf_ptrs =
-        Box::from_iter(iter::repeat_with(JackBufPtr::dangling).take(num_ports.get()));
+        Box::from_iter(iter::repeat_with(JackBufPtr::dangling).take(n_ports.get()));
 
     let writer_async_client = jack::contrib::ClosureProcessHandler::new(move |_client, scope| {
         let Some(frames) = num::NonZeroUsize::new(scope.n_frames() as usize) else {
@@ -153,8 +152,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             *ptr = JackBufPtr::from_slice(port.as_slice(scope));
         }
 
-        let Ok(mut write_chunk) =
-            tx.write_chunk_uninit(num_ports.checked_mul(frames).unwrap().get())
+        let Ok(mut write_chunk) = tx.write_chunk_uninit(n_ports.checked_mul(frames).unwrap().get())
         else {
             return jack::Control::Continue;
         };
@@ -181,7 +179,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         unsafe { write_chunk.commit_all() };
 
-        if rb_size_spls.get() - tx.slots() >= chunk_size_spls.get() {
+        if rb_size_spls - tx.slots() >= chunk_size_spls.get() {
             network_thread.unpark();
         }
 
@@ -191,18 +189,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let _async_client = jack_client.activate_async((), writer_async_client).unwrap();
 
     loop {
-
         let Ok(read_chunk) = rx.read_chunk(chunk_size_spls.get()) else {
             std::thread::park();
             continue;
         };
 
-        // The other half is empty since RB_SIZE % CHUNK_SIZE = 0
+        // the ring buffer's size is a multiple of chunk_size_spls, so the second slice is empty
         let (slice, _) = read_chunk.as_slices();
 
-        let slice = as_bytes(slice);
-
-        socket.send_to(slice, &addr).unwrap();
+        socket.send(as_bytes(slice)).unwrap();
 
         read_chunk.commit_all();
     }
